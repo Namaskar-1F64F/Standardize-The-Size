4c9e2d2fa41fdf37cfd7d9ece516b0cb25a5b5dc
Kyle <kyletrusler@gmail.com>
2017-01-28 11:31:59 -0500

    changed url

--- code/static/js/app/app.js
+++ code/static/js/app/app.js
@@ -3,7 +3,7 @@ uploadFile =function() {
 
     $.ajax({
         type: "POST",
-        url: "http://23b35a31.ngrok.io/uploads/"+file.name,
+        url: "http://344fae2a.ngrok.io/uploads/"+file.name,
         data: file,
         processData: false,
         contentType: false,
