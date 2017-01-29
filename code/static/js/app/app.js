0fe58083b9b029407d011dd6b04e3e950a7e24a2
Kyle <kyletrusler@gmail.com>
2017-01-28 19:58:26 -0500

    added blimp and fixed dimension calculation

--- code/static/js/app/app.js
+++ code/static/js/app/app.js
@@ -1,15 +1,18 @@
-uploadFile =function() {
-    var file    = document.querySelector('input[type=file]').files[0];
-
-    $.ajax({
-        type: "POST",
-        url: "http://cfe81af2.ngrok.io",
-
-        files : file,
-        processData: false,
-        contentType: false,
-        success: function (data) {
-            console.log('yo');    }
+$(function() {
+    $('#upload-file-btn').click(function() {
+        var form_data = new FormData($('#file')[0]);
+        $.ajax({
+            type: 'POST',
+            url: 'http://cfe81af2.ngrok.io/',
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            async: false,
+            success: function(data) {
+                var last = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
+                window.location.href = window.location.href.replace(last, "profile.html?w="+data.substring(2,7)+"&h="+data.substring(19,24));
+            }
+        });
     });
-//d
-}
+});
